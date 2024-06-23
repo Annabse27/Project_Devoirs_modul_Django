4ebc87708eccb453eda248a1e46f51bce4f6ee62
Annabse27 <annabse27@gmail.com>
2024-06-23 07:17:15 +0200

    version complète : code HTML et style CSS sont séparés

--- app.py
+++ app.py
@@ -4,7 +4,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    """
+    Page d'accueil de l'application.
+    Cette fonction renvoie le template de la page d'accueil du site de catalogue de produits.
+    """
     return render_template('index.html')
 
 if __name__ == '__main__':
+
+    """
+    Point d'entrée de l'application Flask.
+    L'application s'exécute en mode debug.
+    """
+
     app.run(debug=True)
+
